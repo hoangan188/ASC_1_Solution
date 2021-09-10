7da156610aa5791c45b03f67a33044ef5285e332
An <123456@gmail.com>
2021-09-10 11:47:13 +0700

    First commit

--- ASC_1/wwwroot/js/init.js
+++ ASC_1/wwwroot/js/init.js
@@ -7,7 +7,7 @@
       document.addEventListener('DOMContentLoaded', function () {
           var elems = document.querySelectorAll('.collapsible');
           var instances = M.Collapsible.init(elems, options);
-      });
+      });  
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
