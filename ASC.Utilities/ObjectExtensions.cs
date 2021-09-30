fcb0e4766cf88f77615e8015a5952636651798b9
An <123456@gmail.com>
2021-09-30 23:20:59 +0700

    Setup AzureStorage Chapter4 (DataAccess,Utlities,Models)

---
+++ ASC.Utilities/ObjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+namespace ASC.Utilities
+{
+    public static class ObjectExtensions
+    {
+        public static T CopyObject<T>(this object objSource)
+        {
+            var serialized = JsonConvert.SerializeObject(objSource);
+            return JsonConvert.DeserializeObject<T>(serialized);
+        }
+    }
+}
